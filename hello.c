ecb8a70a178f8285ec5a4427163fec1e26c56e9a
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-08-25 03:10:18 +1000

    conditionals

--- hello.c
+++ hello.c
@@ -2,16 +2,33 @@
 
 int main(void) 
 {
-    char name[50];
-    int age;
+    // Accepting and returning the value
+    // char name[50];
+    // int age;
 
-    printf("What's your name? ");
-    scanf("%s", name);  
+    // printf("What's your name? ");
+    // scanf("%s", name);  
 
-    printf("Hello, %s! How old are you? ", name);  
-    scanf("%d", &age);  
+    // printf("Hello, %s! How old are you? ", name);  
+    // scanf("%d", &age);  
 
-    printf("Nice to meet you, %s. You are %d years old.\n", name, age);  
+    // printf("Nice to meet you, %s. You are %d years old.\n", name, age);  
+
+
+    // conditionals
+
+    int x, y;
+
+    printf("Enter the value of x: ");
+    scanf("%i", &x);
+    printf("Enter the value of y: ");
+    scanf("%i", &y);
+
+    if(x<y)
+    {
+        printf("%i Is greater than %i",x,y);
+
+    }
     
     return 0;
 }
