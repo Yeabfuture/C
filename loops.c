f71185a7cd24358696336ee4b8676e35255008ae
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-08-27 17:30:23 +1000

    for and do while loop

--- loops.c
+++ loops.c
@@ -4,19 +4,29 @@ int main(void)
 {
     // for loop
 
+    for(int i=0; i<3; i ++) 
+    {
+        printf("Meow from the for loop\n");
+    }
 
-    // // while loop
+    // while loop
 
-    // int counter = 3;
+    int counter = 3;
 
-    // while(counter > 0)
-    // {
-    //     printf("Meow \n");
-    //     counter--;
-    // }
+    while(counter > 0)
+    {
+        printf("Meow while loop\n");
+        counter--;
+    }
 
+    do while loop
 
-    // do while loop
+    int counter =3;
+    do 
+    {
+        printf("Meow from do while loop \n");
+        counter--;
+    } while(counter >0);
 
     return 0;
 }
