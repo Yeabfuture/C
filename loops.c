4eac2adba704ab37b64f83c338585e0149104483
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-08-27 17:18:57 +1000

    while loop

---
+++ loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    // for loop
+
+
+    // // while loop
+
+    // int counter = 3;
+
+    // while(counter > 0)
+    // {
+    //     printf("Meow \n");
+    //     counter--;
+    // }
+
+
+    // do while loop
+
+    return 0;
+}
