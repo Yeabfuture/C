910b21b08d6745f3fbbd79d99db72e478b36ef86
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-09-10 05:44:41 +0800

    projects done

---
+++ me/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name [30];
+    printf("What is Your name? ");
+    scanf("%s", name);
+    printf("Hello, %s \n !", name);
+    return 0;
+
+    // submit50 cs50/problems/2024/x/me
+}
